c373c0ed7defe559d4335ee4f2d15ed34fc21d4d
Sơn Nguyễn Kỳ Duyên <skyduyen13726@gmail.com>
2025-01-28 17:27:22 +0700

    Add files via upload

--- Bai004_Struct_Time.cpp
+++ Bai004_Struct_Time.cpp
@@ -1,1 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Time{
+	int hours,minutes,seconds;
+};
+
+void Chuanhoa(Time &x){
+	x.minutes += x.seconds / 60; //quy doi giay -> phut
+    x.seconds %= 60; 
+
+    if (x.seconds < 0) {
+        x.seconds += 60;
+        x.minutes--;
+    }
+
+    x.hours += x.minutes / 60; //quy doi phut -> gio
+    x.minutes %= 60;
+
+    if (x.minutes < 0) {
+        x.minutes += 60;
+        x.hours--;
+    }
+
+    x.hours %= 24;			 // chuan hoa gio
+    if (x.hours < 0) {
+        x.hours += 24;
+    }
+}
+
+Time addTime(int x, int y, int z, Time current){
+	Time after=current;
+	after.seconds += z;
+    after.minutes += y;
+    after.hours += x;
+    Chuanhoa(after);
+	return after;	
+}
+
+int main(){
+	Time current,after;
+	cout << "Nhap thoi gian hien tai (gio phut giay): \n";
+    cin >> current.hours >> current.minutes >> current.seconds;
+    int x,y,z;
+    cout << "Nhap so gio, phut, giay can cong them: \n"; 
+    cin >> x>> y >> z;
+    after = addTime (x,y,z,current);
+    cout << "Thoi gian sau khi cong them la: \n";
+    cout << after.hours << " gio, " << after.minutes << " phut, " << after.seconds << " giay.\n";
+	return 0;
+}
